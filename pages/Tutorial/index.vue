9d7a5ba4b5c445aa0e60de69f76360f2ab009df4
kapilsomani.h@gmail.com <3639200+capsci@users.noreply.github.com>
2020-11-18 20:46:59 -0500

    Get dad jokes from provider using axios

--- pages/Tutorial/index.vue
+++ pages/Tutorial/index.vue
@@ -5,8 +5,42 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    
+    data() {
+        return {
+            jokes: [],
+        }
+    },
+    async created() {
+        const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+        try {
+            const res = await axios.get('https://icanhazdadjoke.com/search', config);
+            this.jokes = res.data.results;
+        } catch (err) {
+            console.log(err)
+        }
+
+        
+    },
+    head() {
+        return {
+            title: "Tutorial - Dad Jokes",
+            // meta data is good for SEO
+            meta: [
+                {
+                    hid: "tut",
+                    name: "tut",
+                    content: "Tut for things I'm trying",
+                }
+            ],
+        }
+    }
 }
 </script>
 

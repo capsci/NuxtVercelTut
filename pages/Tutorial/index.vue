3d237e9fb730756bf80ead7347b5cd8cd56f2426
kapilsomani.h@gmail.com <3639200+capsci@users.noreply.github.com>
2020-11-18 20:22:30 -0500

    Add pages (and routes are created automatically)

---
+++ pages/Tutorial/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        This is my tutorial page    
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>My Nuxt Vercel App</h1>
+        <p>Trying my first Nuxt App to be deployed on Vercel</p>
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: "About the app",
+            // meta data is good for SEO
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "My tutorial to use Nuxt with Vercel",
+                }
+            ],
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
